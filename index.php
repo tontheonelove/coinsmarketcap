458f9feff5ac0f4424731022858d42b5178bedb7
tontheonelove <tonlove5233@gmail.com>
2017-11-15 10:55:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,5 +16,9 @@ p {
 <p><script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="ethereum" data-base="THB" data-secondary="" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div></p>
 <p><script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="bitcoin-cash" data-base="THB" data-secondary="" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div></p>
 
+</table>
+
+
+
 </body>
 </html>
